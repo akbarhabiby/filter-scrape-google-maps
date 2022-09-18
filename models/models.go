f33a56473939a6c844a5e59b39e85d1d9b878b7e
Muhammad Akbar Habiby Khalid <mail@akbarhabiby.net>
2022-09-18 20:09:52 +0700

    Modified model

--- models/models.go
+++ models/models.go
@@ -3,18 +3,18 @@ package models
 import "time"
 
 type Scrapes struct {
-	ID          int         `json:"id"`
-	FullAddress string      `json:"full_address"`
-	Number      string      `json:"number"`
-	District    string      `json:"district"`
-	City        string      `json:"city"`
-	Province    string      `json:"province"`
-	PostalCode  string      `json:"postal_code"`
-	Country     string      `json:"country"`
-	Latitude    string      `json:"latitude"`
-	Longitude   string      `json:"longitude"`
-	PlusCode    interface{} `json:"plus_code"`
-	CreatedAt   time.Time   `json:"created_at"`
+	ID          int       `json:"-"`
+	FullAddress string    `json:"full_address"`
+	Number      string    `json:"number"`
+	District    string    `json:"district"`
+	City        string    `json:"city"`
+	Province    string    `json:"province"`
+	PostalCode  string    `json:"postal_code"`
+	Country     string    `json:"country"`
+	Latitude    string    `json:"latitude"`
+	Longitude   string    `json:"longitude"`
+	PlusCode    string    `json:"plus_code"`
+	CreatedAt   time.Time `json:"-"`
 }
 
 type Result struct {
