e4311a9354b0f9ba1852fd7aee4b264900b9d3ee
Muhammad Akbar Habiby Khalid <mail@akbarhabiby.net>
2022-09-24 18:39:53 +0700

    Modified ExportJSONtoCSV to export multiple csv with max 1e+6

--- helpers/csv.go
+++ helpers/csv.go
@@ -3,38 +3,51 @@ package helpers
 import (
 	"encoding/csv"
 	"fmt"
+	"math"
 	"os"
+	"strings"
 
 	"github.com/akbarhabiby/filter-scrape-google-maps/models"
 )
 
 func ExportJSONtoCSV(fileName string, jsonData interface{}) {
-	fmt.Printf("Exporting %s ...\n", fileName)
-	timeLog := Timelog(fmt.Sprintf("%s %s", "Export", fileName))
-
 	rows, ok := jsonData.([]models.Scrapes)
 	if !ok {
 		panic("Failed to parse jsonData to rows")
 	}
 
-	file, err := os.Create(fileName)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
+	maxLoop := int(1e+6) // * For handling MS Excel max rows
+	totalLoop := 0
+	rowsLength := len(rows)
 
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-
-	if err := writer.Write([]string{"full_address", "number", "district", "city", "province", "postal_code", "country", "latitude", "longitude", "plus_code"}); err != nil {
-		panic(err)
-	}
-
-	for _, row := range rows {
-		if err := writer.Write([]string{row.FullAddress, row.Number, row.District, row.City, row.Province, row.PostalCode, row.Country, row.Latitude, row.Longitude, row.PlusCode}); err != nil {
+	for i := 0; i < int(math.Ceil(float64(rowsLength)/float64(maxLoop))); i++ {
+		fName := fmt.Sprintf("%s-%v.csv", strings.ReplaceAll(fileName, ".csv", ""), i)
+		fmt.Printf("Exporting %s ...\n", fName)
+		timeLog := Timelog(fmt.Sprintf("%s %s", "Export", fName))
+		tempLoop := 0
+		file, err := os.Create(fName)
+		if err != nil {
 			panic(err)
 		}
-	}
+		defer file.Close()
 
-	timeLog()
+		writer := csv.NewWriter(file)
+		defer writer.Flush()
+
+		if err := writer.Write([]string{"full_address", "number", "district", "city", "province", "postal_code", "country", "latitude", "longitude", "plus_code"}); err != nil {
+			panic(err)
+		}
+
+		for {
+			if err := writer.Write([]string{rows[totalLoop].FullAddress, rows[totalLoop].Number, rows[totalLoop].District, rows[totalLoop].City, rows[totalLoop].Province, rows[totalLoop].PostalCode, rows[totalLoop].Country, rows[totalLoop].Latitude, rows[totalLoop].Longitude, rows[totalLoop].PlusCode}); err != nil {
+				panic(err)
+			}
+			tempLoop++
+			totalLoop++
+			if tempLoop == maxLoop || totalLoop == rowsLength {
+				timeLog()
+				break
+			}
+		}
+	}
 }
